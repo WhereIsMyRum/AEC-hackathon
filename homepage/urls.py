69dc4f80282ca234562f15e1393802b583f07e55
unknown <Piotr@kopipw10.emea.SAS.com>
2019-01-26 17:17:35 +0100

    edited html files

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name="home_page"
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('products/', views.products, name='products'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'homepage/index.html')
+
+def about(request):
+    return render(request, 'homepage/about.html')
+
+def products(request):
+    return render(request, 'homepage/products.html')
 # Create your views here.
